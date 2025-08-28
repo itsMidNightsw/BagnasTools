b0cccff4ed54a8af7840f6a158923ca5897344a3
unknown <adam.bernard22@gmail.com>
2025-08-28 13:03:46 +0200

    Cleaning up first training part

--- Classification/RF_Classification.R
+++ Classification/RF_Classification.R
@@ -1,4 +1,7 @@
 
 
+####### ENTRAINEMENT RANDOM FOREST ########
 
-source(file = paste(getwd(),"/Classification/rf_refined_full_dataset_2017to2023_4bands.R", sep = ""), local = F, echo = T)
+source(file = paste(getwd(),"/Classification/rf_refined_full_dataset_2017to2023_4bands.R", sep = ""), local = F, echo = F)
+
+

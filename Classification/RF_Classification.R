91dd7b8f4dd49de7a5bccf70f40ec187f2b40e2b
unknown <adam.bernard22@gmail.com>
2025-09-02 14:37:22 +0200

    sourcing classif script

--- Classification/RF_Classification.R
+++ Classification/RF_Classification.R
@@ -4,4 +4,5 @@
 
 source(file = paste(getwd(),"/Classification/rf_refined_full_dataset_2017to2023_4bands.R", sep = ""), local = F, echo = F)
 
+source(file = paste(getwd(),"/Classification/classify_simple_image_4bands_V2.R", sep = ""), local = F, echo = F)
 

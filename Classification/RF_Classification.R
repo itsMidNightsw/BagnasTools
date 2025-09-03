3c9b014526136874b960c81ea977ec5057e5d3ca
unknown <adam.bernard22@gmail.com>
2025-09-03 14:55:58 +0200

    load function source

--- Classification/RF_Classification.R
+++ Classification/RF_Classification.R
@@ -4,5 +4,8 @@
 
 source(file = paste(getwd(),"/Classification/rf_refined_full_dataset_2017to2023_4bands.R", sep = ""), local = F, echo = F)
 
+
+####### LOAD FUNCTION ########
+
 source(file = paste(getwd(),"/Classification/classify_simple_image_4bands_V2.R", sep = ""), local = F, echo = F)
 

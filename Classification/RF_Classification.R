2ded5acae39a1712432e6e0fde56c63150850162
unknown <adam.bernard22@gmail.com>
2025-09-04 14:06:11 +0200

    wrong path

--- Classification/RF_Classification.R
+++ Classification/RF_Classification.R
@@ -1,11 +1,13 @@
 
 
 ####### ENTRAINEMENT RANDOM FOREST ########
-
 source(file = paste(getwd(),"/Classification/rf_refined_full_dataset_2017to2023_4bands.R", sep = ""), local = F, echo = F)
 
 
 ####### LOAD FUNCTION ########
-
 source(file = paste(getwd(),"/Classification/classify_simple_image_4bands_V2.R", sep = ""), local = F, echo = F)
+
+
+###### CLASS ALL FOLDER IMAGES ######
+source(file = paste(getwd(),"/Classification/classify_multi_images_4bands_V2.R", sep = ""), local = F, echo = F)
 
